b13932bb33351f3268b38c5499433ee978449c09
nathanjchan <nathanjchan2@gmail.com>
2024-06-18 15:51:27 -0700

    Add is_channel_live

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,28 @@
 import { describe, it, expect } from 'vitest';
+import is_channel_live from '../src/lib/is_channel_live';
 
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
 		expect(1 + 2).toBe(3);
 	});
 });
+
+describe('is_channel_live test', () => {
+	it('should return a boolean for PewDiePie', async () => {
+		expect(typeof (await is_channel_live('PewDiePie'))).toBe('boolean');
+	});
+
+	it('should return true for LofiGirl', async () => {
+		expect(await is_channel_live('LofiGirl')).toBe(true);
+	});
+
+	it('should return false for honeybbqglaze5522', async () => {
+		expect(await is_channel_live('honeybbqglaze5522')).toBe(false);
+	})
+
+	it('should throw an error for a non-existent channel', async () => {
+		await expect(is_channel_live(''))
+			.rejects.toThrow('Failed to fetch https://youtube.com/@ with status 404.');
+	})
+});
+ 
---
+++ src/lib/is_channel_live.ts
@@ -0,0 +1,17 @@
+function does_text_contain(text: string, search: string): boolean {
+	return text.indexOf(search) !== -1;
+}
+
+function is_channel_live(channel_id: string): Promise<boolean> {
+	return new Promise(async (resolve, reject) => {
+		const url = `https://youtube.com/@${channel_id}`;
+		const response = await fetch(url);
+    if (response.status !== 200) {
+      reject(`Failed to fetch ${url} with status ${response.status}.`)
+    }
+		const text = await response.text();
+		resolve(does_text_contain(text, `"text":" watching"`));
+	});
+}
+
+export default is_channel_live;
