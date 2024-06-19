0a1f4481ba46ab4997e26a155b366c9d89143f1f
nathanjchan <nathanjchan2@gmail.com>
2024-06-19 12:27:08 -0700

    Move YT request to server

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,14 @@
+import get_channels from '$lib/get_channels';
+import is_channel_live from '$lib/is_channel_live';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
+	let channels = [];
+	channels = await get_channels();
+	type status = { [key: string]: boolean };
+	let channel_status: status = {};
+	for (const channel of channels) {
+		channel_status[channel] = await is_channel_live(channel);
+	}
+	return { channel_status };
+}
