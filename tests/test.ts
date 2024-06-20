d660853d61fccd66fb85912ddd551f5212c863c7
nathanjchan <nathanjchan2@gmail.com>
2024-06-19 17:33:00 -0700

    Add Playwright tests

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,21 @@ test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.locator('h1')).toBeVisible();
 });
+
+test('home page has expected h2', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.locator('h2')).toHaveCount(2);
+})
+
+test('home page has expected links', async ({ page }) => {
+	await page.goto('/');
+	const links = page.locator('a');
+	let count = 0;
+	for (let i = 0; i < (await links.count()); i++) {
+		const href = await links.nth(i).getAttribute('href');
+		if (href && href.startsWith('https://www.youtube.com/@StDominicsSanFrancisco')) {
+			count++;
+		}
+	}
+	expect(count).toBeGreaterThan(0);
+});
